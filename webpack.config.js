1f557ea0cc98ab9885a6c52113d5c782165ee655
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-20 11:25:52 -0500

    fix generate files with extentions hot.js hot.map and hot.json

--- webpack.config.js
+++ webpack.config.js
@@ -7,120 +7,128 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 let Env = "local";
 
 let settings = {
-    entry: {
-        app: "./src/index.ts",
-    },
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "js/[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true,
-                        },
-                    },
-                ],
+	entry: {
+		app: "./src/index.ts",
+	},
+	output: {
+		path: path.join(__dirname, "public"),
+		filename: "js/[name].js",
+        clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader",
+						options: {
+							transpileOnly: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							// Prefer `dart-sass`
+							implementation: require("sass"),
+						},
+					},
+				],
+			},
+		],
+	},
+	// devtool: "eval-cheap-module-source-map",
+	mode: "development",
+	devServer: {
+        hot: true,
+		open: {
+			app: {
+				name: "chrome",
+				arguments: ["--incognito", "--new-window"],
+			},
+		},
+		host: "localhost",
+		// static: {
+		// directory: path.join(__dirname,'..', 'public'),
+		// },
+		compress: true,
+		port: 9000,
+		devMiddleware: {
+			// writeToDisk: true,
+            writeToDisk: (filePath) => {
+                return /^(?!.*(hot)).*/.test(filePath);
             },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            // Prefer `dart-sass`
-                            implementation: require("sass"),
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    // devtool: 'eval-cheap-module-source-map',
-    mode: "development",
-    devServer: {
-        open: {
-            app: {
-                name: 'chrome',
-                arguments: ['--incognito', '--new-window'],
-            },
-        },
-        host: "localhost",
-        // static: {
-      	// directory: path.join(__dirname,'..', 'public'),
-        // },
-        compress: true,
-        port: 9000,
-        devMiddleware: {
-            writeToDisk: true
-        }
-    },
-    resolve: {
-        enforceExtension: false,
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-    },
-    plugins: [
-        
-        new ProgressPlugin({
-            activeModules: false,
-            entries: true,
-            handler(percentage, message, ...args) {
-                // custom logic
-            },
-            modules: true,
-            modulesCount: 5000,
-            profile: false,
-            dependencies: true,
-            dependenciesCount: 10000,
-            percentBy: null,
-        }),
-        
-        new HtmlWebpackPlugin({
-            chunks: ["app"],
-            template: "./src/html/index.html",
-            //filename:"index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/[name].css",
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-              { from: "src/assets", to: "assets" },
-            //   { from: "other", to: "public" },
-            ],
-          }),
-    ],
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-                    name: "vendor",
-                    chunks: "all",
-                },
-            },
-        },
-    },
+		},
+	},
+	resolve: {
+		enforceExtension: false,
+		extensions: [".ts", ".tsx", ".js", ".jsx"],
+	},
+	plugins: [
+		new ProgressPlugin({
+			activeModules: false,
+			entries: true,
+			handler(percentage, message, ...args) {
+				// custom logic
+			},
+			modules: true,
+			modulesCount: 5000,
+			profile: false,
+			dependencies: true,
+			dependenciesCount: 10000,
+			percentBy: null,
+		}),
+
+		new HtmlWebpackPlugin({
+			chunks: ["app"],
+			template: "./src/html/index.html",
+			//filename:"index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: "src/assets", to: "assets" },
+				//   { from: "other", to: "public" },
+			],
+		}),
+	],
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+					name: "vendor",
+					chunks: "all",
+				},
+			},
+		},
+	},
 };
 
 module.exports = (env, argv) => {
-    if (argv.env.NODE_ENV == "production")
-        settings.module.rules[2].use[0] = MiniCssExtractPlugin.loader;
+	if (argv.env.NODE_ENV == "production")
+		settings.module.rules[2].use[0] = MiniCssExtractPlugin.loader;
 
-    if (argv.mode === "production") settings.devtool = "source-map";
+	if (argv.mode === "production") {
+		settings.devtool = "source-map";
+	}else{
+        settings.devtool = "source-map";
+    }
 
-    return settings;
+	return settings;
 };
