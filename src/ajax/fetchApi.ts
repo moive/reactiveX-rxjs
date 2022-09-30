e6af5f20bd85632b3997c1bd54c25534dca16732
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-29 23:27:42 -0500

    ajax Rxjs and catchError

--- src/ajax/fetchApi.ts
+++ src/ajax/fetchApi.ts
@@ -1,5 +1,8 @@
+import { ajax, AjaxError } from "rxjs/ajax";
+import { catchError, map, of } from "rxjs";
+
 export default function () {
-	const url = "https://api.github.com/userXXs?per_page=5";
+	const url = "https://api.github.com/userxxs?per_page=5";
 
 	const handleErrors = (response: Response) => {
 		console.log(response);
@@ -12,9 +15,21 @@ export default function () {
 	};
 	const fetchPromise = fetch(url);
 
-	fetchPromise
-		.then(handleErrors)
-		.then((res) => res.json())
-		.then((data) => console.log(data))
-		.catch((err) => console.warn("Error in users", err));
+	// fetchPromise
+	// 	.then(handleErrors)
+	// 	.then((res) => res.json())
+	// 	.then((data) => console.log(data))
+	// 	.catch((err) => console.warn("Error in users", err));
+
+	const handleError = (err: AjaxError) => {
+		console.warn("Error in users", err);
+		return of([]);
+	};
+
+	ajax(url)
+		.pipe(
+			map(({ response }) => response),
+			catchError(handleError)
+		)
+		.subscribe((users) => console.log("users: ", users));
 }
