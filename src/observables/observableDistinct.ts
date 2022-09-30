9efc1ccc50499b4e193455a3f634b17e15894a3c
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-29 21:12:48 -0500

    operator DistinctUntilChanged

--- src/observables/observableDistinct.ts
+++ src/observables/observableDistinct.ts
@@ -1,8 +1,8 @@
-import { distinct, of, from } from "rxjs";
+import { distinct, of, from, distinctUntilChanged } from "rxjs";
 export default function () {
 	const numbers$ = of(1, 1, 1, "1", 2, 2, 3, 3, 3, 4, 4, 1, "1", 3);
 
-	numbers$.pipe(distinct()).subscribe(console.log);
+	numbers$.pipe(distinctUntilChanged()).subscribe(console.log);
 }
 
 interface Person {
@@ -11,15 +11,15 @@ interface Person {
 
 const persons: Person[] = [
 	{ name: "Megaman" },
-	{ name: "X " },
+	{ name: "Megaman" },
 	{ name: "Zero" },
 	{ name: "Dr. Wily" },
-	{ name: "Megaman" },
+	{ name: "X " },
 	{ name: "X " },
 	{ name: "Zero" },
 	{ name: "Dr. Wily" },
 ];
 
 from(persons)
-	.pipe(distinct((x) => x.name))
+	.pipe(distinctUntilChanged((prev, next) => prev.name === next.name))
 	.subscribe(console.log);
