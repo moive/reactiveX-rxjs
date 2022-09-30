6711688a92f7418269eb4ebefed47558b46984c1
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-30 10:55:43 -0500

    typing all operators

--- src/ajax/flattening.ts
+++ src/ajax/flattening.ts
@@ -1,5 +1,7 @@
 import { ajax } from "rxjs/ajax";
-import { debounceTime, fromEvent, map, mergeAll } from "rxjs";
+import { debounceTime, fromEvent, map, mergeAll, Observable } from "rxjs";
+import { GithubUser } from "../interfaces/github-user.interface";
+import { GithubUsersResp } from "../interfaces/github-users.interface";
 export default function () {
 	const textInput = document.createElement("input");
 
@@ -9,13 +11,15 @@ export default function () {
 
 	input$
 		.pipe(
-			debounceTime(500),
-			map((ev) => (ev.target as HTMLInputElement).value),
-			map((text) =>
+			debounceTime<KeyboardEvent>(500),
+			map<KeyboardEvent, string>(
+				(ev) => (ev.target as HTMLInputElement).value
+			),
+			map<string, Observable<GithubUsersResp>>((text) =>
 				ajax.getJSON(`https://api.github.com/search/users?q=${text}`)
 			),
-			mergeAll(),
-			map(({ items }: any) => items)
+			mergeAll<Observable<GithubUsersResp>>(),
+			map<GithubUsersResp, GithubUser[]>(({ items }) => items)
 		)
-		.subscribe(console.log);
+		.subscribe((res) => console.log(res));
 }
---
+++ src/interfaces/github-users.interface.ts
@@ -0,0 +1,6 @@
+import { GithubUser } from "./github-user.interface";
+export interface GithubUsersResp {
+	total_count: number;
+	incomplete_results: boolean;
+	items: GithubUser[];
+}
