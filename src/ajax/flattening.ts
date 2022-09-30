417402390c9a74fdf80f182c32d6503bd234393d
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-30 11:22:43 -0500

    show data in html

--- src/ajax/flattening.ts
+++ src/ajax/flattening.ts
@@ -1,11 +1,33 @@
 import { ajax } from "rxjs/ajax";
-import { debounceTime, fromEvent, map, mergeAll, Observable } from "rxjs";
+import { debounceTime, fromEvent, map, mergeAll, Observable, tap } from "rxjs";
 import { GithubUser } from "../interfaces/github-user.interface";
 import { GithubUsersResp } from "../interfaces/github-users.interface";
 export default function () {
 	const textInput = document.createElement("input");
+	const orderList = document.createElement("ol");
+	document.querySelector("body")?.append(textInput, orderList);
 
-	document.querySelector("body")?.append(textInput);
+	// Helpers
+	const showUsers = (users: GithubUser[]) => {
+		orderList.innerHTML = "";
+
+		for (const user of users) {
+			const li = document.createElement("li");
+			const img = document.createElement("img");
+			const anchor = document.createElement("a");
+
+			img.src = user.avatar_url;
+			anchor.href = user.html_url;
+			anchor.text = "View Page";
+			anchor.target = "_blank";
+
+			li.append(img);
+			li.append(user.login + " ");
+			li.append(anchor);
+
+			orderList.append(li);
+		}
+	};
 
 	const input$ = fromEvent<KeyboardEvent>(textInput, "keyup");
 
@@ -21,5 +43,5 @@ export default function () {
 			mergeAll<Observable<GithubUsersResp>>(),
 			map<GithubUsersResp, GithubUser[]>(({ items }) => items)
 		)
-		.subscribe((res) => console.log(res));
+		.subscribe(showUsers);
 }
