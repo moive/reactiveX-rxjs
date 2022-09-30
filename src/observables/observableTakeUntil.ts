2042d28fc0d0b61d4eb7eea259867456593f41fe
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-29 20:40:21 -0500

    operator Skip

--- src/observables/observableTakeUntil.ts
+++ src/observables/observableTakeUntil.ts
@@ -1,11 +1,16 @@
-import { interval, takeUntil, fromEvent } from "rxjs";
+import { interval, takeUntil, fromEvent, skip, tap } from "rxjs";
 export default function () {
 	const button = document.createElement("button");
 	button.textContent = "Stop timer";
 
 	document.querySelector("body")?.append(button);
 
-	const click$ = fromEvent(button, "click");
+	// const click$ = fromEvent(button, "click");
+	const click$ = fromEvent(button, "click").pipe(
+		tap(() => console.log("before...")),
+		skip(1),
+		tap(() => console.log("after..."))
+	);
 
 	const counter$ = interval(1000)
 		.pipe(takeUntil(click$))
