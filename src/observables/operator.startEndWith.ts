750708be951e91aa586b614217da59627d5f12a1
Moises Velasquez <mvelasquez@pictoric.com>
2022-10-04 20:39:08 -0500

    endWith

--- src/observables/operator.startEndWith.ts
+++ src/observables/operator.startEndWith.ts
@@ -1,6 +1,6 @@
-import { of, startWith } from "rxjs";
+import { endWith, of, startWith } from "rxjs";
 export default function () {
-	const number$ = of(...[1, 2, 3, 4]).pipe(startWith(0));
+	const number$ = of(...[1, 2, 3, 4]).pipe(startWith(0), endWith(5, 6, 7));
 
 	number$.subscribe(console.log);
 }
