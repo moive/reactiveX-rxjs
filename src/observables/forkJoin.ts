55814c2fb3dc158b7b9eec15857fbd4f00844c90
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-11-16 23:31:46 -0500

    fork-join case study

--- src/observables/forkJoin.ts
+++ src/observables/forkJoin.ts
@@ -1,8 +1,22 @@
-import { of, interval, take, forkJoin } from "rxjs";
+import { of, interval, take, forkJoin, catchError } from "rxjs";
+import { ajax } from "rxjs/ajax";
 export default function () {
 	const num$ = of(1, 2, 3, 4);
 	const int$ = interval(1000).pipe(take(3));
 	const let$ = of("a", "b", "c");
 
 	forkJoin({ num: num$, int: int$, let: let$ }).subscribe(console.log);
+
+	const GITHUB_API_URL = "https://api.github.com/users";
+	const GITHUB_USER = "moive";
+
+	forkJoin({
+		user: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}`),
+		repos: ajax
+			.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/repo123`)
+			.pipe(catchError((err) => of([]))),
+		gists: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/gists`),
+	})
+		.pipe(catchError((e) => of(e)))
+		.subscribe(console.log);
 }
